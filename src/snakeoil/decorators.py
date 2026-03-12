a2afe20bee6065fe0cc16be335ff96d77feb62f3
Brian Harring <ferringb@gmail.com>
2026-03-12 18:24:29 +0100

    decorators coroutines: annotate
    Signed-off-by: Brian Harring <ferringb@gmail.com>

--- src/snakeoil/decorators.py
+++ src/snakeoil/decorators.py
@@ -1,13 +1,23 @@
 """Various decorator utilities."""
 
 from functools import wraps
+from typing import Callable, Generator, ParamSpec, TypeVar
+
+P = ParamSpec("P")
+T_yield = TypeVar("T_yield")
+T_send = TypeVar("T_send")
+T_return = TypeVar("T_return")
 
 
-def coroutine(func):
+def coroutine(
+    func: Callable[P, Generator[T_yield, T_send, T_return]],
+) -> Callable[P, Generator[T_yield, T_send, T_return]]:
     """Prime a coroutine for input."""
 
     @wraps(func)
-    def prime(*args, **kwargs):
+    def prime(
+        *args: P.args, **kwargs: P.kwargs
+    ) -> Generator[T_yield, T_send, T_return]:
         cr = func(*args, **kwargs)
         next(cr)
         return cr
